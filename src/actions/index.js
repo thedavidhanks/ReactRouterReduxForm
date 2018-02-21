eb616369e48e06f947ce3de1521996e306f41a81
David Hanks <davidhanks@gmail.com>
2018-02-21 17:17:24 -0600

    Modern React with Redux - Lecture 148

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POST = 'fetch_post';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POSTS = 'create_posts';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=dphanks0849';
@@ -32,4 +33,12 @@ export function fetchPost(id){
         type: FETCH_POST,
         payload: request
     };
+}
+
+export function deletePost(id, callback){
+	const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`).then(() => callback());;
+	return {
+		type: DELETE_POST,
+		payload: id
+	}
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -12,26 +12,25 @@ class PostsIndex extends Component{
 		return _.map(this.props.posts, post => {
 			return (
 				<li className="list-group-item" key={post.id}>
-					{post.title}
+					<Link to={`/posts/${post.id}`}>{post.title}</Link>
 				</li>
 			);
 		});
 	}
 	render() {
-		console.log(this.props.posts);
 		return (
-				<div>
-					<div className="text-xs-right">
-						<Link className="btn btn-primary" to="/posts/new">Add a Post </Link>
-					</div>
-					<h3>Posts</h3>
-					<ul className="list-group">
-						{this.renderPosts()}
-					</ul>
+			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">Add a Post </Link>
 				</div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
+			</div>
 		);
     }
- }
+ };
 
 function mapStateToProps(state){
 	return{ posts: state.posts};
