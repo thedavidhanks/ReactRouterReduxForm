52bfa658d1f95ec1e472832644385bac346daf79
David Hanks <davidhanks@gmail.com>
2018-02-22 14:19:13 -0600

    Modern React with Redux - Lecture 151

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,7 +5,7 @@ export default function (state = {}, action) {
 	switch(action.type){
 		case DELETE_POST:
 			return _.omit(state, action.payload);
-		case FETCH_POSTS:
+		case FETCH_POST:
 			return { ...state, [action.payload.data.id]: action.payload.data };
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, "id");
